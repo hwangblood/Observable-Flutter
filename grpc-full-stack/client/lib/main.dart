2b419f041ec3c3fb911adfac5c4e49ee90eca5fb
hwangblood <hwangblood@gmail.com>
2023-08-19 00:06:46 +0800

    grpc: gRPC ClientChannel & TodoServiceClient

--- grpc-full-stack/client/lib/main.dart
+++ grpc-full-stack/client/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:protos/protos.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -9,10 +11,84 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return const HomePage();
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late ClientChannel _channel;
+  late TodoServiceClient _stub;
+
+  int _counter = 0;
+  Todo? todo;
+
+  @override
+  void initState() {
+    super.initState();
+    _channel = ClientChannel(
+      'localhost',
+      port: 8080,
+      options: const ChannelOptions(
+        credentials: ChannelCredentials.insecure(),
+      ),
+    );
+
+    _stub = TodoServiceClient(_channel);
+
+    _fetchTodo();
+  }
+
+  @override
+  void dispose() {
+    _channel.shutdown();
+    super.dispose();
+  }
+
+  Future<void> _fetchTodo() async {
+    setState(() {
+      _counter++;
+    });
+
+    _stub.getTodo(GetTodoByIdRequest(id: _counter)).then(
+          (p0) => setState(
+            () => todo = p0,
+          ),
+        );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Full Stack with gRPC'),
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: todo == null
+              ? Text(
+                  'No data yet',
+                  style: Theme.of(context)
+                      .textTheme
+                      .displayMedium
+                      ?.copyWith(color: Colors.red),
+                )
+              : Text(
+                  todo!.toString(),
+                  style: Theme.of(context).textTheme.displayMedium,
+                ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _fetchTodo,
+          child: const Icon(Icons.refresh),
         ),
       ),
     );
--- grpc-full-stack/protos/example/protos_example.dart
+++ grpc-full-stack/protos/example/protos_example.dart
@@ -2,6 +2,7 @@ import 'package:protos/protos.dart';
 
 void main() {
   final todo = Todo(id: 1, title: 'first todo', completed: false);
+  print(todo);
   print(todo.toProto3Json());
   print(todo.writeToJsonMap());
 }
